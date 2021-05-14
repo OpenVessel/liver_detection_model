b3442665bf3cc6b5e2d9bb9597906b6f50669ba4
Gresliebear <lesliemwubbel@gmail.com>
2021-05-14 11:36:45 -0400

    First update of model code, preprocess

---
+++ slice_classification_v1.py
@@ -0,0 +1,120 @@
+###
+# Model to sort slices either liver or not liver
+#
+#
+#
+# 4/11/2021
+# Vaibhav Gupta
+
+##########
+## Img size is now 512,512 so that implementation into the rest of the model is easier
+## The idea here is to call one function to other models as preprocess all functions defined here 
+## will be called through def model_call_function():
+############## import statements 
+
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import RMSprop
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+import pandas as pd
+import datetime
+
+def slice_classification(train_imgs_path, test_imgs_path, val_imgs_path, save_model_path):
+    
+    #images are extracted from the path or folder
+    train = ImageDataGenerator( rescale = 1./255 ) 
+    #print(type(train))                                     #HERE IS WHERE THE ISSUE MAY BE???
+    validation = ImageDataGenerator( rescale = 1./255 )
+    #print(type(validation)) 
+    train_dataset = train.flow_from_directory(train_imgs_path,
+                                            shuffle = True, 
+                                            target_size = (512, 512),
+                                            batch_size = 32,
+                                            class_mode = 'binary',
+                                            seed = 44)
+    class_names = train_dataset.class_indices
+    print(class_names)
+    validation_dataset = train.flow_from_directory(val_imgs_path,
+                                            shuffle = True,      
+                                            target_size = (512, 512), #input shape here
+                                            batch_size = 32,
+                                            class_mode = 'binary',
+                                            seed = 44)
+    print(train_dataset) ## binary class 
+    print(validation_dataset) # binary class 
+    ## building the model
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (512, 512, 3)),
+        tf.keras.layers.MaxPool2D(2,2),
+        tf.keras.layers.Conv2D(64, (3,3), activation = "relu"),
+        tf.keras.layers.MaxPool2D(2,2),
+        tf.keras.layers.Conv2D(128, (3,3), activation = "relu"),
+        tf.keras.layers.MaxPool2D(2,2),
+        tf.keras.layers.Conv2D(128, (3,3), activation = "relu"),
+        tf.keras.layers.MaxPool2D(2,2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation = 'relu'),
+        tf.keras.layers.Dense(1, activation = 'sigmoid')
+    ])
+    model.save(save_model_path)
+    ### record model BCE loss
+    ## metric accuracy 
+    model.compile(loss = 'binary_crossentropy',
+                optimizer = RMSprop(lr = 1e-4),
+                metrics = ['accuracy'])
+    start = datetime.datetime.now()
+    fitted_model = model.fit(train_dataset,
+                        steps_per_epoch = 2,
+                        epochs = 30,
+                        validation_data = validation_dataset,
+                        validation_freq = 1,
+                        #callbacks = [tensorboard, early_stopping]
+                            )
+    end = datetime.datetime.now()
+    elapsed = end - start
+    print('\n ---------Elapsed Time-----------')
+    print('Time to fit baseline model is:\n {}'.format(elapsed))
+    print("-------Model Summary-------")
+    model.summary()
+    
+    ## Citation 
+    # https://machinelearningspace.com/yolov3-tensorflow-2-part-4/
+    # model = tf.keras.models.load_model(r'C:\Users\12673\Desktop\Projects\OpenVessel\liverseg-2017-nipsws\LiTS_database\images_volumes\Liver PNGs from matlab seperated\ClassificationModel')
+    # class_names = {'Liver': 0, 'Non-Liver': 1}
+    # for i in os.listdir(test_imgs_path):
+        
+    #     img = image.load_img(test_imgs_path + '\\' + i, target_size = (512,512))
+    #     # plt.imshow(img)
+    #     # plt.show()
+    #     X = image.img_to_array(img)
+    #     X = np.expand_dims(X, axis = 0)
+    #     images = np.vstack([X])
+    #     val = model.predict(images)
+    #     # if val[0] == 0:
+    #     #     print("Liver")
+        
+    #     # score = tf.nn.softmax(val[0])
+    #     # print(np.argmax(score))
+    #     print(
+    #         "This image most likely belongs to {} with a {:.2f} percent confidence."
+    #         .format(class_names[np.argmax(val)], 100 * np.max(val))
+    #     )
+
+
+
+def model_call_function():
+    ## calls other functions in this script to be called into other scripts 
+    train_imgs_path = r"C:\Users\12673\Desktop\Projects\OpenVessel\liverseg-2017-nipsws\LiTS_database\images_volumes\LiverPNGsfrommatlabseperated\Train"
+    val_imgs_path = r"C:\Users\12673\Desktop\Projects\OpenVessel\liverseg-2017-nipsws\LiTS_database\images_volumes\LiverPNGsfrommatlabseperated\Validation"
+    test_imgs_path = r"C:\Users\12673\Desktop\Projects\OpenVessel\liverseg-2017-nipsws\LiTS_database\images_volumes\LiverPNGsfrommatlabseperated\Test\Liver"
+    save_model_path = r"C:\Users\12673\Desktop\Projects\OpenVessel\liverseg-2017-nipsws\LiTS_database\images_volumes\LiverPNGsfrommatlabseperated\ClassificationModel"
+    slice_classification(train_imgs_path, test_imgs_path, val_imgs_path, save_model_path)
+model_call_function()
+# path = r"C:\Users\12673\Desktop\Projects\OpenVessel\liverseg-2017-nipsws\LiTS_database\images_volumes\Liver PNGs from matlab seperated\Train\Liver\704127.png"
+# x = np.asarray(Image.open(path))
+# print(type(x[0][0][0]))
---
+++ utils/preprocess_database_liver.py
@@ -0,0 +1,114 @@
+##
+
+
+
+
+#Proir arthors 
+#Nathan Reilly 
+#Gregory Glatzer 
+
+#5/14/2021 
+#Leslie Wubbel 
+###
+
+import os
+import nibabel as nib # pip install nibabel  # https://nipy.org/nibabel/nifti_images.html
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+from PIL import Image
+import time 
+start = time.time()
+# path constants
+niftis_path = 'E:\Datasets\LiTS_liver_lesion\LITS17'
+root_process_database = '../../output_folder/'
+
+## WINDOWS STRINGS
+# niftis_path = r'E:\Datasets\LiTS_liver_lesion\LITS17'
+# root_process_database = r'D:\L_pipe\liver_open\liverseg-2017-nipsws\output_folder'
+
+
+## Folders to be created
+folder_volumes = os.path.join(root_process_database, 'images_volumes/')
+folder_seg_liver = os.path.join(root_process_database, 'liver_seg/')
+folder_seg_item = os.path.join(root_process_database, 'item_seg/')
+
+
+# create non-existent paths
+folder_paths = [root_process_database, folder_volumes, folder_seg_liver, folder_seg_item]
+    
+for p in folder_paths:
+    if not os.path.exists(p):
+        os.mkdir(p)
+
+
+# filter to only files starting with v (volume) or s (segmentation)
+filenames = [filename for filename in os.listdir(niftis_path) if filename[0] in ('v', 's')]
+
+
+for filename in filenames:
+    path_file = os.path.join(niftis_path, filename)
+    index = filename.find('.nii') + 1 # +1 to account for matlab --> python
+    if filename[0] == 'v':
+        print('Processing Volume {}'.format(filename))
+        folder_volume = os.path.join(folder_volumes, filename[7:index-1])
+        volume = nib.load(path_file) # load nifti
+        imgs = volume.get_fdata() # get 3d NumPy array
+
+        # clipping HU pixel clipping 
+        imgs[imgs<-150] = -150
+        imgs[imgs>250] = 250
+
+        ## # equivalent to matlab single()
+        imgs = imgs.astype(np.float32) 
+        img_max, img_min = (np.max(imgs), np.min(imgs))
+
+        # create folder_volume folder
+        img_volume = 255*(imgs - img_min)/(img_max-img_min)
+        if not os.path.exists(folder_volume):
+            os.mkdir(folder_volume)
+        
+        for k in range(img_volume.shape[2]):
+            section = img_volume[:,:,k]
+            filename_for_section = os.path.join(folder_volume, str(k+1) + '.mat')
+            scipy.io.savemat(filename_for_section, {'section': section})
+    else:
+        print('Processing Segmentation {}'.format(filename))
+        folder_seg_item_num = os.path.join(folder_seg_item, filename[13:index-1])
+        folder_seg_liver_num = os.path.join(folder_seg_liver, filename[13:index-1])
+        segmentation = nib.load(path_file)
+        img_seg = segmentation.get_fdata().astype(np.uint8)
+        print(img_seg.shape)
+        # binarize and normalize data
+        img_seg_item = img_seg.copy()
+        img_seg_liver = img_seg.copy()
+
+        # create masks
+        img_seg_item[img_seg_item == 1] = 0
+        img_seg_item[img_seg_item == 2] = 1
+        img_seg_liver[img_seg_liver == 2] = 1
+
+        # create dirs
+        if not os.path.exists(folder_seg_item_num):
+            os.mkdir(folder_seg_item_num)
+        if not os.path.exists(folder_seg_liver_num):
+            os.mkdir(folder_seg_liver_num)
+        
+        # save images
+        for k in range(0, img_seg_item.shape[2]):
+            print(filename, ", ", k)
+            # item
+            item_seg_section = np.fliplr(np.flipud(img_seg_item[:,:,k]*255)) # flip on both axes
+            item_seg_filename = os.path.join(folder_seg_item_num, str(k+1) + '.png')
+            im_item = Image.fromarray(item_seg_section)
+            im_item.save(item_seg_filename)
+            
+            # liver
+            liver_seg_section = np.fliplr(np.flipud(img_seg_liver[:,:,k]*255))
+            liver_seg_filename = os.path.join(folder_seg_liver_num, str(k+1) + '.png')
+            im_liver = Image.fromarray(liver_seg_section)
+            im_liver.save(liver_seg_filename)
+
+
+end = time.time()
+print("Elapsed Time is:", end - start)
